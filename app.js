c1eecfe914a15656376a1a5bdf254ca36a8f8569
193Eric <373986529@qq.com>
2017-03-17 15:44:30 +0800

    updata

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
