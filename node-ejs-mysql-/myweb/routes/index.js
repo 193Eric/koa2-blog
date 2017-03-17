3d804966997bbd2000a0db155c791131e603e8d6
kaili <kai.li@easoncomm.com>
2017-03-17 15:30:50 +0800

    updata

--- node-ejs-mysql-/myweb/routes/index.js
+++ node-ejs-mysql-/myweb/routes/index.js
@@ -1,24 +1,29 @@
 var express = require('express')
 var router = express.Router()
+var url = require('url')
 var mysql = require('mysql')
-var conn = mysql.createConnection({
+/*var conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '123456',
   database: 'node_web',
   port: 3306
 })
-conn.connect()
+conn.connect()*/
 /* GET home page. */
-
-var nav
+/*var nav
 conn.query('select * from nav', function (err, rows, fields) {
   if (err) throw err
-  nav = rows
-})
+  nav = rows;
+})*/
 /*获取导航栏*/
 router.get('/', function (req, res, next) {
-  res.render('index', { nav: nav, title: 'Express'})
+    res.render('index', { nav: "nav" })
+})
+router.get('/book', function (req, res, next) {
+  var arg = url.parse(req.url, true).query
+  console.log(arg.title)
+  res.render('book', { title: 'Express',img: '/images/page.jpg',name: arg.title})
 })
 router.get('/edit', function (req, res, next) {
   res.render('ueditor', { title: 'Express' })
