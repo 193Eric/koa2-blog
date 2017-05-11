44620afdaa24af9bddb6641ea5653734b0dca079
kaili <373986529@qq.com>
2017-05-11 18:44:40 +0800

    路由懒加载

--- src/router.js
+++ src/router.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import homePage from './views/home.vue'
+/*import homePage from './views/home.vue'
 import blog from './views/blog.vue'
 import login from './views/login.vue'
-import index from './views/index.vue'
+import index from './views/index.vue'*/
+const homePage = resolve => require(['./views/home.vue'], resolve)
+const blog = resolve => require(['./views/blog.vue'], resolve)
+const login = resolve => require(['./views/login.vue'], resolve)
+const index = resolve => require(['./views/index.vue'], resolve)
 Vue.use(Router)
 
 export default new Router({
