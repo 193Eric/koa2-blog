61579f6598ea9bff525d7b37ac1e2f8e2cd274d2
kaili <373986529@qq.com>
2017-07-18 14:53:23 +0800

    update

--- api/connect.js
+++ api/connect.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql')
 var fs = require('fs')
+var path = require('path');
 var dbData,sql
-var content = fs.readFileSync('./../config/mysql.json')
+var content = fs.readFileSync(path.join(__dirname, "./../config/mysql.json"))
 sql = mysql.createConnection(JSON.parse(content.toString()))
 module.exports = sql
